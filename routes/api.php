d79e779a73accf3d5e279089cfb2b51999ad66d4
darektoa <darektoa@gmail.com>
2022-01-18 00:21:44 +0700

    Added: Create school API route

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,17 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// AUTH
 Route::prefix('auth')->group(function() {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
+});
+
+
+// WITH AUTHENTICATION
+Route::middleware(['auth.api'])->group(function() {
+    // SCHOOL
+    Route::prefix('school')->group(function() {
+        Route::post('/create', [SchoolController::class, 'store']);
+    });
 });
