6196348670e3e18a04c627304ba393ff0fad652c
darektoa <darektoa@gmail.com>
2022-01-17 11:24:00 +0700

    Added: User and school relation method in school and user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,4 +22,9 @@ class User extends Authenticatable
     protected $casts    = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function school() {
+        return $this->hasOne(School::class);
+    }
 }
