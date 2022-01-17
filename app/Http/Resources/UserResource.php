3b500c906383042b0ee409956b728e899d3b67f3
darektoa <darektoa@gmail.com>
2022-01-18 00:06:58 +0700

    Added: School property in user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -22,6 +22,7 @@ class UserResource extends JsonResource
             'email'         => $this->email,
             'created_at'    => $this->created_at,
             'updated_at'    => $this->updated_at,
+            'school'        => SchoolResource::make($this->whenLoaded('school')),
         ];
     }
 }
