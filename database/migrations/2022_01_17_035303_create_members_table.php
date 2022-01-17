abec221958243d803e305a1c86b1bece4e981d9e
darektoa <darektoa@gmail.com>
2022-01-17 11:04:16 +0700

    Added: Members table migration

---
+++ database/migrations/2022_01_17_035303_create_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('school_id')->constrained();
+            $table->foreignId('role_id')->constrained('member_roles');
+            $table->bigInteger('balance')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
