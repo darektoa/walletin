b6c5f4b044df854bb8bb60fec2230620fdcad29b
darektoa <darektoa@gmail.com>
2022-01-17 11:39:32 +0700

    Fixed: Sender_id and receiver_id field in transactions table migration

--- database/migrations/2022_01_17_041724_create_transactions_table.php
+++ database/migrations/2022_01_17_041724_create_transactions_table.php
@@ -15,8 +15,8 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('sender_id')->nullable()->constrained();
-            $table->foreignId('receiver_id')->constrained();
+            $table->foreignId('sender_id')->nullable()->constrained('users');
+            $table->foreignId('receiver_id')->constrained('users');
             $table->string('code', 16)->unique();
             $table->bigInteger('amount');
             $table->smallInteger('status')->default(1);
