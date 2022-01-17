0b94e34e4264a0d7a988baabbb24d082a766b478
darektoa <darektoa@gmail.com>
2022-01-17 22:28:48 +0700

    Added: Auth login API route

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function() {
+    Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
 });
