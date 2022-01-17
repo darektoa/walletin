98b87f8d6f5cabd668ee98df5bbbf1cbe4fa8ad1
darektoa <darektoa@gmail.com>
2022-01-17 21:04:40 +0700

    Added: User resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'            => $this->id,
+            'token'         => $this->when($this->token, $this->token),
+            'name'          => $this->name,
+            'username'      => $this->username,
+            'email'         => $this->email,
+            'created_at'    => $this->created_at,
+            'updated_at'    => $this->updated_at,
+        ];
+    }
+}
