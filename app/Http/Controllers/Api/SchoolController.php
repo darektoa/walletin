9dc9e129e6c53345cf63038fd44f0ae071e2f3c0
darektoa <darektoa@gmail.com>
2022-01-18 00:20:14 +0700

    Added: Store method in school API controller

--- app/Http/Controllers/Api/SchoolController.php
+++ app/Http/Controllers/Api/SchoolController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Exceptions\ErrorException as Error;
+use App\Helpers\{ResponseHelper, RandomCodeHelper};
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class SchoolController extends Controller
 {
-    
+    public function store(Request $request) {
+        try{
+            $user  = User::find(auth()->id());
+
+            $user->school->firstOrCreate([], [
+                'code'  => RandomCodeHelper::make()
+            ]);
+
+            return ResponseHelper::make(
+                UserResource::make($user)
+            );
+        }catch(Error $err) {
+            return ResponseHelper::error(
+                $err->getErrors(),
+                $err->getMessage(),
+                $err->getCode(),
+            );
+        }
+    }
 }
