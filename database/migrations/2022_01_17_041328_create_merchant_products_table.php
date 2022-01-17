07399d456de6d628204174641cbca50cfcc17ef6
darektoa <darektoa@gmail.com>
2022-01-17 11:16:12 +0700

    Added: Merchant products table migration

---
+++ database/migrations/2022_01_17_041328_create_merchant_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMerchantProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merchant_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('merchant_id')->constrained();
+            $table->string('name');
+            $table->bigInteger('price');
+            $table->smallInteger('quantity');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('merchant_products');
+    }
+}
