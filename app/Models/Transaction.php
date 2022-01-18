55f7abdf2b8042b1bab13fad0c66e58953afa4f4
darektoa <darektoa@gmail.com>
2022-01-18 10:24:42 +0700

    Added: Get status name attribute in transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -20,4 +20,21 @@ class Transaction extends Model
     public function receiver() {
         return $this->belongsTo(User::class, 'receiver_id');
     }
+
+
+    public function getStatusName() {
+        $status = $this->status;
+        $name   = null;
+
+        switch($status) {
+            case 1: $name = 'Pending'; break;
+            case 2: $name = 'Paid'; break;
+            case 3: $name = 'Expired'; break;
+            case 4: $name = 'Success'; break;
+            case 5: $name = 'Failed'; break;
+            default: $name = 'Unknown';
+        }
+
+        return $name;
+    }
 }
