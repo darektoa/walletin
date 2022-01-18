b2efa61f9df283eb7d16c024b1da2b303d75181e
darektoa <darektoa@gmail.com>
2022-01-18 07:20:46 +0700

    Added: Call member role seeder in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             SchoolSeeder::class,
+            MemberRoleSeeder::class,
             MemberSeeder::class,
             MerchantSeeder::class,
         ]);
