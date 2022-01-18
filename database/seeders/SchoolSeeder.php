2f28f27d239622facb2ae345cac0f86a98f50c2b
darektoa <darektoa@gmail.com>
2022-01-18 07:08:21 +0700

    Edited: Rename school variable to user

--- database/seeders/SchoolSeeder.php
+++ database/seeders/SchoolSeeder.php
@@ -10,15 +10,15 @@ class SchoolSeeder extends Seeder
 {
     public function run()
     {
-        $school = User::create([
+        $user = User::create([
             'name'      => 'School',
             'email'     => 'school@email.test',
             'username'  => 'school',
             'password'  => Hash::make('password'),
         ]);
 
-        $school->school()->create();
-        $school->tokens()->create([
+        $user->school()->create();
+        $user->tokens()->create([
             'name'      => 'user',
             'abilities' => ['*'],
             'token'     => 'ee5c8da7f361881470ffe290d6362c60bf398364d449f40b652da74ea5bd4932' // 2|bX1c5dCsrHRkJNJRxWlbInkbYsiiZmQa7YoRdEWq
