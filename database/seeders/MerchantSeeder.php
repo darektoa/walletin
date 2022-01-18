b3179a53a97477f8e7aa4ec03465ff98650ee54f
darektoa <darektoa@gmail.com>
2022-01-18 07:11:06 +0700

    Added: Merchant seeder

---
+++ database/seeders/MerchantSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class MerchantSeeder extends Seeder
+{
+    public function run()
+    {
+        $user = User::create([
+            'name'      => 'Merchant',
+            'email'     => 'merchant@email.test',
+            'username'  => 'merchant',
+            'password'  => Hash::make('password'),
+        ]);
+
+        $user->merchant()->create();
+        $user->tokens()->create([
+            'name'      => 'user',
+            'abilities' => ['*'],
+            'token'     => '12844264728736c1f50b6dc9aa7bd854305981cf8fbffa4b6c99da08020a5404' // 4|wVs7Kp5V2EgqEk1vQoUsI6AV1d12Y3Q1PZmpigcg
+        ]);
+    }
+}
