561a5ccc40970cf96c721d7e925b7c25aceee66d
darektoa <darektoa@gmail.com>
2022-01-18 07:11:23 +0700

    Added: Call merchant seeder in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             SchoolSeeder::class,
             MemberSeeder::class,
+            MerchantSeeder::class,
         ]);
 
         
