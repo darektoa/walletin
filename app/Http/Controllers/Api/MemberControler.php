74b1a004ae997458c4df8657f933972f18d2ad16
darektoa <darektoa@gmail.com>
2022-01-19 08:42:44 +0700

    Added: Using API request validator trait in store method of member API controller

--- app/Http/Controllers/Api/MemberControler.php
+++ app/Http/Controllers/Api/MemberControler.php
@@ -7,31 +7,25 @@ use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use App\Models\{Member, School, User};
+use App\Traits\Api\RequestValidator;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class MemberControler extends Controller
 {
+    use RequestValidator;
+
     public function store(Request $request) {
         try{
-            $validator = Validator::make($request->all(), [
+            $this->validate($request, [
                 'school_code'   => 'required|exists:schools,code',
             ]);
-
-            if($validator->fails()) {
-                $errors = $validator->errors()->all();
-                throw new Error('Unprocessable', 422, $errors);
-            }
 
             $code   = $request->school_code;
             $school = School::findCode($code);
             $user   = User::find(auth()->id());
 
-            Member::joinSchool($user, $school);
-            
-            $user->load([
-                'member.school'
-            ]);
+            Member::joinSchool($user, $school);            
+            $user->load(['member.school']);
 
             return ResponseHelper::make(
                 UserResource::make($user)
