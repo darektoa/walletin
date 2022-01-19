ede7982a2988d0b88e75c2b002737bc31b45469d
darektoa <darektoa@gmail.com>
2022-01-19 08:42:30 +0700

    Added: API request validator trait

---
+++ app/Traits/Api/RequestValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits\Api;
+
+use App\Exceptions\ErrorException as Error;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+trait RequestValidator{
+    protected function validate(
+        Request $request,
+        array $rules,
+        array $message=[],
+        array $customAttribute=[]
+    ) {
+        $validator = Validator::make(
+            $request->all(),
+            $rules,
+            $message,
+            $customAttribute,
+        );
+
+        if($validator->fails()) {
+            $errors = $validator->errors()->all();
+            return throw new Error('Unprocessable', 422, $errors);
+        }
+
+        return $validator;
+    }
+}
