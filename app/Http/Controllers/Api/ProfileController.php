04f5bcef84745d9c285e306e0fa68c4ef7096b58
darektoa <darektoa@gmail.com>
2022-01-20 11:33:11 +0700

    Added: Index method in profile API controller

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    
+    public function index(Request $request) {
+        $user = User::with(['school', 'member', 'merchant'])
+            ->find(auth()->id());
+
+        return ResponseHelper::make(
+            UserResource::make($user)
+        );
+    }
 }
