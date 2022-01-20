15258faba60e64ba0116c2afaa5a90ec3ceeaec0
darektoa <darektoa@gmail.com>
2022-01-20 10:58:02 +0700

    Added: Index method in school member API controller

--- app/Http/Controllers/Api/SchoolMemberController.php
+++ app/Http/Controllers/Api/SchoolMemberController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+Use App\Http\Resources\MemberResource;
+use App\Models\{Member, User};
 use Illuminate\Http\Request;
 
 class SchoolMemberController extends Controller
 {
-    //
+    public function index(Request $request) {
+        $perPage    = (int) $request->per_page;
+        $user       = User::with(['school'])->find(auth()->id());
+        $members    = Member::with(['role', 'user'])
+            ->where('school_id', $user->school->id)
+            ->paginate($perPage);
+
+        return ResponseHelper::paginate(
+            MemberResource::collection($members)
+        );
+    }
 }
