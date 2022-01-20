57e4a796a1dcad10e523e9e2a99bda6f0e5f341d
darektoa <darektoa@gmail.com>
2022-01-20 11:17:04 +0700

    Added: Index method in school merchant API controller

--- app/Http/Controllers/Api/SchoolMerchantController.php
+++ app/Http/Controllers/Api/SchoolMerchantController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\MerchantResource;
+use App\Models\{Merchant, User};
 use Illuminate\Http\Request;
 
 class SchoolMerchantController extends Controller
 {
-    //
+    public function index(Request $request) {
+        $perPage    = (int) $request->per_page;
+        $user       = User::with(['school'])->find(auth()->id());
+        $members    = Merchant::with(['user'])
+            ->where('school_id', $user->school->id)
+            ->paginate($perPage);
+
+        return ResponseHelper::paginate(
+            MerchantResource::collection($members)
+        );
+    }
 }
